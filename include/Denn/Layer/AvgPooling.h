12623052865355c9aeb91b5bace94e2c1012d715
Gabriele91 <dbgabri@gmail.com>
2019-02-16 12:49:13 +0100

    Added Avg pooling

---
+++ include/Denn/Layer/AvgPooling.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "Denn/Config.h"
+#include "Denn/Layer.h"
+#include "Denn/Utilities/Convolution.h"
+
+namespace Denn
+{
+	class AvgPooling : public PoolingLayer
+	{
+	public:
+		///////////////////////////////////////
+		/// Constructor
+		///
+		/// \param in_width      Width of the input image in each channel.
+		/// \param in_height     Height of the input image in each channel.
+		/// \param in_channels   Number of input channels.
+		/// \param window_width  Width of the filter.
+		/// \param window_height Height of the filter.
+		/// \param stride        Kernel stride (x, y).
+		///
+		AvgPooling 
+		(
+			  int in_width, int in_height, int in_channels
+			, int window_width, int window_height, int stride = 1
+		);
+		AvgPooling 
+		(
+			  const Shape& in
+			, const std::vector<int>& metadata
+		);
+		//////////////////////////////////////////////////
+		virtual Layer::SPtr copy() const override;
+		//////////////////////////////////////////////////
+		virtual const Inputs inputs() const override;
+		//////////////////////////////////////////////////
+		virtual const Matrix& feedforward(const Matrix& input) override;
+		virtual const Matrix& backpropagate(const Matrix& prev_layer_data, const Matrix& next_layer_data) override;
+		//////////////////////////////////////////////////
+		virtual void update(const Optimizer& optimize) override;
+		//////////////////////////////////////////////////
+	protected:    
+		//shape conv
+		internal::ConvDims m_dim;		           // dimensions of convolution
+	};
+
+	REGISTERED_LAYER(
+		AvgPooling,
+		LAYER_NAMES("avg_pooling", "avgp"),
+		LayerShapeType(SHAPE_2D_3D),    //shape 2D/3D
+		LayerDescription::MinMax{ 2,3 } //min args filter size, max args filter size + stride
+	)
+}
---
+++ source/Layer/AvgPooling.cpp
@@ -0,0 +1,138 @@
+#include "Denn/Layer/AvgPooling.h"
+#define _metadata(_x, _default) (_x < metadata.size() ? metadata[_x] : _default)
+#define _shapepooling(in, kernel, stride) (1 + std::ceil((in - kernel) / stride))
+
+namespace Denn
+{
+    AvgPooling::AvgPooling 
+    (
+      int in_width, int in_height, int in_channels
+    , int window_width, int window_height, int stride
+    )
+	: PoolingLayer("max_pooling",
+		Shape(in_width, in_height, in_channels),
+		Shape(_shapepooling(in_width, window_width, stride), 
+			  _shapepooling(in_height, window_height, stride),
+			  in_channels))
+    , m_dim(in_width, in_height, in_channels, window_width, window_height, in_channels, stride, 0, 0)
+    {
+
+    }
+    AvgPooling::AvgPooling 
+    (
+      const Shape& in
+    , const std::vector<int>& metadata
+    )
+	: AvgPooling
+	(
+	//shapes
+	  in.width(), in.height(), in.channels()
+	//inputs
+	, metadata[0], metadata[1], _metadata(2, 1)
+    )
+    {
+        
+    }
+    //////////////////////////////////////////////////
+	Layer::SPtr AvgPooling::copy() const
+	{
+		return std::static_pointer_cast<Layer>(std::make_shared<AvgPooling>(*this));
+	}
+	const Inputs AvgPooling::inputs() const
+	{
+		return 
+		{ 
+			m_dim.width_kernel, m_dim.height_kernel, m_dim.stride
+		};
+	}	
+    //////////////////////////////////////////////////
+    const Matrix& AvgPooling::feedforward(const Matrix& bottom)
+    {
+        int n_sample = bottom.cols();
+        int hw_in = m_dim.in_image_size();
+        int hw_pool = m_dim.pool_size();
+        int hw_out = m_dim.out_image_size();
+        //alloc and init
+        m_top.resize(int(out_size()), n_sample);
+        m_top.setZero(); 
+        //avg pooling
+        for (int i = 0; i < n_sample; i ++) 
+        {
+            auto image = bottom.col(i);
+            for (int c = 0; c < m_dim.channel_in; c ++) 
+            {
+                for (int i_out = 0; i_out < hw_out; i_out ++) 
+                {
+                    int step_h = i_out / m_dim.width_out;
+                    int step_w = i_out % m_dim.width_out;
+                    // left-top idx of window in raw image
+                    int start_idx = step_h * m_dim.width_in * m_dim.stride + step_w * m_dim.stride;
+                    for (int i_pool = 0; i_pool < hw_pool; i_pool++) 
+                    {
+                        if (start_idx % m_dim.width_in + i_pool % m_dim.width_kernel >= m_dim.width_in ||
+                            start_idx / m_dim.width_in + i_pool / m_dim.width_kernel >= m_dim.height_in) 
+                        {
+                            continue;  // out of range
+                        }
+                        //index
+                        int pick_idx = start_idx + (i_pool / m_dim.width_kernel) *
+                                       m_dim.width_in + i_pool % m_dim.width_kernel + c * hw_in;
+                        //test
+                        if (image(pick_idx) >= m_top(c * hw_out + i_out, i)) 
+                        {  // max pooling
+                            m_top(c * hw_out + i_out, i) += image(pick_idx);
+                        }
+                    }
+                    m_top(c * hw_out + i_out, i) /= hw_pool;
+                }
+            }
+        }
+        return m_top;
+    }
+    
+    const Matrix& AvgPooling::backpropagate(const Matrix& bottom, const Matrix& grad) 
+    {
+        int n_sample = bottom.cols();
+        int hw_in = m_dim.in_image_size();
+        int hw_pool = m_dim.pool_size();
+        int hw_out = m_dim.out_image_size();
+        m_grad_bottom.resize(bottom.rows(), n_sample);
+        m_grad_bottom.setZero();
+        //dy/dx = 1/n
+        //dout = 1/n * din
+        //avg pooling
+        for (int i = 0; i < n_sample; i ++) 
+        {
+            for (int c = 0; c < m_dim.channel_in; c ++) 
+            {
+                for (int i_out = 0; i_out < hw_out; i_out ++) 
+                {
+                    int step_h = i_out / m_dim.width_out;
+                    int step_w = i_out % m_dim.width_out;
+                    // left-top idx of window in raw image
+                    int start_idx = step_h * m_dim.width_in * m_dim.stride + step_w * m_dim.stride;
+                    for (int i_pool = 0; i_pool < hw_pool; i_pool++) 
+                    {
+                        if (start_idx % m_dim.width_in + i_pool % m_dim.width_kernel >= m_dim.width_in ||
+                            start_idx / m_dim.width_in + i_pool / m_dim.width_kernel >= m_dim.height_in) 
+                        {
+                            continue;  // out of range
+                        }
+                        //index
+                        int pick_idx = start_idx + (i_pool / m_dim.width_kernel) *
+                                       m_dim.width_in + i_pool % m_dim.width_kernel + c * hw_in;
+                        //test
+                        m_grad_bottom(pick_idx, i) += grad(c * hw_out + i_out, i) / hw_pool;
+                    }
+                }
+            }
+        }
+        return m_grad_bottom;
+    }
+    
+	void AvgPooling::update(const Optimizer& optimize)
+	{
+        /* none */
+	}
+    //////////////////////////////////////////////////
+}
