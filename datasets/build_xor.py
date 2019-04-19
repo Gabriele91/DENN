21d66c31919f2a1d8931ce95931f587c0ea41381
Gabriele91 <dbgabri@gmail.com>
2019-04-19 12:20:26 +0200

    Update xor dataset generator

--- datasets/build_xor.py
+++ datasets/build_xor.py
@@ -2,9 +2,12 @@ import os
 from DennDBLib.creator import nbit_parity 
 
 def main():
-    for nbit in range(5,13):        
+    for nbit in range(5,15):        
         print("Start generation: xor{}.db.gz".format(nbit))
-        nbit_parity("xor{}.db".format(nbit), nbit=nbit, validation_all_values=True, test_all_values=True, batch_size=1000)
+        nbit_parity("xor{}.db".format(nbit), nbit=nbit, 
+        validation_all_values=False, 
+        test_all_values=False, 
+        batch_size=1000)
 
 if __name__ == '__main__':
     main()
