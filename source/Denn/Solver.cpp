05d898c788f4848f5aae2188ddb019b5df6a8ae5
Gabriele91 <dbgabri@gmail.com>
2019-05-04 11:39:36 +0200

    Fixed seletion phase

--- source/Denn/Solver.cpp
+++ source/Denn/Solver.cpp
@@ -75,22 +75,22 @@ namespace Denn
 	bool Solver::loss_function_compare(Scalar left, Scalar right) const
 	{
 		return  m_loss_function->minimize() 
-		?  left < right
-		:  right < left
+		?  left <= right
+		:  right <= left
 		;
 	}
 	bool Solver::validation_function_compare(Scalar left, Scalar right) const
 	{
 		return  m_validation_function->minimize() 
-		?  left < right
-		:  right < left
+		?  left <= right
+		:  right <= left
 		;
 	}
 	bool Solver::test_function_compare(Scalar left, Scalar right) const
 	{
 		return  m_test_function->minimize() 
-		?  left < right
-		:  right < left
+		?  left <= right
+		:  right <= left
 		;
 	}
 
