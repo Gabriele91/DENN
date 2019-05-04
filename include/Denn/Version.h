49a199bb182985bda42b4022af29322619eb573e
Gabriele91 <dbgabri@gmail.com>
2019-05-04 10:22:51 +0200

    Update version

--- include/Denn/Version.h
+++ include/Denn/Version.h
@@ -2,9 +2,9 @@
 #define VALUE_TO_STR(s) #s
 #define DEFINE_TO_STR(s) VALUE_TO_STR(s)
 
-#define DENN_VER_MAJOR   1
-#define DENN_VER_MINOR   3
-#define DENN_VER_PATCH   7
+#define DENN_VER_MAJOR   2
+#define DENN_VER_MINOR   1
+#define DENN_VER_PATCH   0
 
 #define DENN_VER_STR \
 		DEFINE_TO_STR(DENN_VER_MAJOR) "." \
