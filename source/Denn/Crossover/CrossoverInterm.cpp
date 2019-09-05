3241a0ac9d393aeeecf23bf8fbd5ae23d7e66139
Gabriele91 <dbgabri@gmail.com>
2019-09-05 22:21:38 +0200

    Added crossover interm2

--- source/Denn/Crossover/CrossoverInterm.cpp
+++ source/Denn/Crossover/CrossoverInterm.cpp
@@ -26,4 +26,26 @@ namespace Denn
 	};
 	REGISTERED_CROSSOVER(Interm, "interm")
 
+	class Interm2 : public Crossover
+	{
+	public:
+		Interm2(const EvolutionMethod& method) : Crossover(method) {}
+
+		virtual void operator()
+		(
+			size_t id_target,
+			Individual& output
+		)
+		{
+			auto w_target = parent(id_target).array();
+			auto w_mutant = output.array();
+			//CROSS
+			for (decltype(w_target.size()) e = 0; e != w_target.size(); ++e)
+			{
+				w_mutant(e) = (Scalar(1)-output.cr()) * w_target(e) + output.cr() * w_mutant(e);
+			}
+		}
+	};
+	REGISTERED_CROSSOVER(Interm2, "interm2")
+
 }
